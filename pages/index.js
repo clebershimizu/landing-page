484d7d52b24d90df5fd67a12d9120838bd431282
Cleber Shimizu <cleber.shimizu@gmail.com>
2021-02-02 14:52:23 -0300

    Adicionadas rotas

--- pages/index.js
+++ pages/index.js
@@ -1,56 +1,10 @@
+import Link from 'next/link';
 function Home() {
   return <>
-    <h1>Saudades do tempo</h1>
-    <h3>Maneva</h3>
-    <p>Saudades do tempo, dos velhos momentos</p>
-    <p>Dos anos passados que foram com o vento</p>
-    <p>  Sorrisos, lembranças, belos sentimentos
-      De transformações e de renascimentos
-      Praias, viagens pela madrugada
-      Nossa rotina era o pé na estrada
-      Sempre felizes sem pensar em nada
-      Paisagem mais bela é o sorriso da amada
-      Contava as estrelas, manto prateado
-      Sentia o calor de um abraço apertado
-      Fazia minha boca tocar o seu lábio
-      Lua iluminava com Bob no rádio
-      Nas manhãs nubladas, bom humor imperava
-      A vida era um jogo, sem cartas marcadas
-      À noite, no fogo, um bom som que rolava
-      Por entre a fumaça, diversas risadas
-      Como se seus ouvidos pudessem respirar
-      O som invadia o corpo, como se fosse o ar
-      O som tomava forma, sensação de bem estar
-      Momentos de magia, muitas formas para amar
-      Marcas de batom na borda de um copo plástico
-      No peito euforia, abraços, riso fácil
-      E com desconhecidos seguia criando laços
-      Transpirava alegria, era dona dos seus passos
-      Consciência admirável, como as tintas de uma tela
-      Seus olhos tinham o brilho das cores da aquarela
-      Seu cabelo ao vento era a paisagem mais bela
-      Tinha a complexidade de uma Vênus moderna
-      Ascendeu ao azul do céu nos seus próprios pensamentos
-      Não pensou no seu futuro, ela era o momento
-      Vi a ponta dos seus pés no gelado do cimento
-      Entre olhares, meu desejo povoar seu pensamento
-      Como se seus ouvidos pudessem respirar
-      O som invadia o corpo, como se fosse o ar
-      O som tomava forma, sensação de bem estar
-      Momentos de magia, muitas formas para amar
-      Marcas de batom na borda de um copo plástico
-      No peito euforia, abraços, riso fácil
-      E com desconhecidos seguia criando laços
-      Transpirava alegria, era dona dos seus passos
-      Consciência admirável, como as tintas de uma tela
-      Seus olhos tinham o brilho das cores da aquarela
-      Seu cabelo ao vento era a paisagem mais bela
-      Tinha a complexidade de uma Vênus moderna
-      Ascendeu ao azul do céu nos seus próprios pensamentos
-      Não pensou no seu futuro, ela era o momento
-      Vi a ponta dos seus pés no gelado do cimento
-      Entre olhares meu desejo, povoar seu pensamento
-      Entre olhares meu desejo, povoar seu pensamento</p>
+    <h1>Home</h1>
+    <Link href="/sobre">
+    <a>Acessar página Sobre</a>
+    </Link>
 </>
 };
 
