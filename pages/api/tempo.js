5037e9ab3410b026c5bbb3b6cd66aadcdd61acff
Cleber Shimizu <cleber.shimizu@gmail.com>
2021-02-03 16:43:00 -0300

    Using stale-while-revalidate

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -4,15 +4,17 @@ async function tempo(request, response){
 
   const apiKey = process.env.CONVERTKIT_API_KEY;
   const apiToken = process.env.CONVERTKIT_API_TOKEN;
-  //conts apiToken
+
   const dynamicDate = new Date();
   const trelloResponse = await fetch(`https://api.trello.com/1/members/me/boards?key=${apiKey}&token=${apiToken}`);
-  
+  const trelloBoards = await fetch(`https://api.trello.com/1/members/me/boards?fields=name,url&key=${apiKey}&token=${apiToken}`);
 
+  response.setHeader('Cache-Control','s-maxage=10, stale-while-revalidate');
   response.json({
     date: dynamicDate.toGMTString(),
-    trello: trelloResponse
+    trello: trelloResponse,
+    boards: trelloBoards
   });
-}
-
+  response.trelloResponse;
+};
 export default tempo;
